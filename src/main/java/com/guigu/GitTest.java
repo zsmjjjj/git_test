3a74227fbde9c0e288214bb81d6512b3611edb45
zsmj <2356353778@qq.com>
2022-03-13 14:37:34 +0800

    master commit3

--- src/main/java/com/guigu/GitTest.java
+++ src/main/java/com/guigu/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("Hello Git3");
         System.out.println("Hello Git4");
         System.out.println("Hot fix Git");
+        System.out.println("Hot fix Git2");
 
     }
 }
