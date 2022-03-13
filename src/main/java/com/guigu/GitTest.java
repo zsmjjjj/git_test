6939aa5dadae453dfcd9a164c44f15d2d18f608d
zsmj <2356353778@qq.com>
2022-03-13 15:15:06 +0800

    SSH

--- src/main/java/com/guigu/GitTest.java
+++ src/main/java/com/guigu/GitTest.java
@@ -1,18 +1,12 @@
 package com.guigu;
 
+/**
+ * @author zsmj
+ */
+
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
-        System.out.println("Hello Git2");
-        System.out.println("Hello Git3");
-        System.out.println("Hello Git4");
-        System.out.println("Hot fix Git");
-        System.out.println("Master Git2");
-        System.out.println("Hot fix Git2");
-        System.out.println("Hot fix Git4");
-        System.out.println("Hot fix Git8");
-        System.out.println("Hot fix Git9");
-
+        System.out.println("Hello SSH");
     }
 }
