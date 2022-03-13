2870feff9a25510db17dde10cf684af1de296582
zsmj <2356353778@qq.com>
2022-03-13 14:40:16 +0800

    Hot commit4

--- src/main/java/com/guigu/GitTest.java
+++ src/main/java/com/guigu/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("Hot fix Git");
         System.out.println("Master Git2");
         System.out.println("Hot fix Git2");
+        System.out.println("Hot fix Git4");
 
     }
 }
