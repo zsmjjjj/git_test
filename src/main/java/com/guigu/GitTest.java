8ac6d243d8b14a7a878e242c884b188c63c6abfb
zsmj <2356353778@qq.com>
2022-03-13 14:34:42 +0800

    hot-fix commit2

--- src/main/java/com/guigu/GitTest.java
+++ src/main/java/com/guigu/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
         System.out.println("Hello Git4");
+        System.out.println("Hot fix Git");
 
     }
 }
