3692b23f12630ec764311d1fa6f04eaa7cbcfdc6
zsmjjjj <101463466+zsmjjjj@users.noreply.github.com>
2022-03-13 15:27:56 +0800

    update code

--- src/main/java/com/guigu/GitTest.java
+++ src/main/java/com/guigu/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello SSH");
         System.out.println("PUll SSHcode");
+        char ado = "ss";
     }
 }
